5de2de4f81560b9c3fe8ac5b39fc75d8a7236e8d
Pinkello <heronavan@gmail.com>
2025-02-24 23:05:33 +0100

    dashboard part1

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,13 +1,9 @@
-import styled from 'styled-components';
-
-import DurationChart from 'features/dashboard/DurationChart';
-import SalesChart from 'features/dashboard/SalesChart';
-import Stats from 'features/dashboard/Stats';
-import TodayActivity from 'features/check-in-out/TodayActivity';
-import { useRecentBookings } from 'features/dashboard/useRecentBookings';
-import Spinner from 'ui/Spinner';
-import { useRecentStays } from './useRecentStays';
-import { useCabins } from 'features/cabins/useCabins';
+import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import { useCabins } from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -23,9 +19,14 @@ We need to distinguish between two types of data here:
 */
 
 function DashboardLayout() {
-  const { isLoading: isLoading1, bookings, numDays } = useRecentBookings();
-  const { isLoading: isLoading2, confirmedStays } = useRecentStays();
-  const { isLoading: isLoading3, cabins } = useCabins();
+  const { bookings, isLoading: isLoading1 } = useRecentBookings();
+  const {
+    stays,
+    confirmedStays,
+    isLoading: isLoading2,
+    numDays,
+  } = useRecentStays();
+  const { cabins, isLoading: isLoading3 } = useCabins();
 
   if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
 
@@ -37,9 +38,9 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <TodayActivity />
-      <DurationChart confirmedStays={confirmedStays} />
-      <SalesChart bookings={bookings} numDays={numDays} />
+      <div>Activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sales</div>
     </StyledDashboardLayout>
   );
 }
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,10 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+import UpdatePassowrdForm from "../features/authentication/UpdatePasswordForm";
 
 function Account() {
   return (
     <>
       <Heading as="h1">Update your account</Heading>
+      <UpdateUserDataForm />
 
       <Row>
         <Heading as="h3">Update user data</Heading>
@@ -13,7 +16,7 @@ function Account() {
 
       <Row>
         <Heading as="h3">Update password</Heading>
-        <p>Update user password form</p>
+        <UpdatePassowrdForm />
       </Row>
     </>
   );
